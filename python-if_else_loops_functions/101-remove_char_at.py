0f9de9923fa221959008ed3bade848176b3cb382
enock04 <e.ntwari@alustudent.com>
2025-02-25 11:06:29 -0800

    advanced tasks

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-
-
 def remove_char_at(str, n):
-    _str = ''
-    for i in str:
-        if i != n:
-            _str = str[:n] + str[n+1:]
-    return _str
-
-
-# print(remove_char_at("qwerty", -2))
+    pst = 0
+    new = ""
+    for char in str:
+        if pst != n:
+            new += char
+        pst += 1
+    return (new)
