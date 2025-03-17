3c5d677f6a2693cfd8240ae73980f9a6f7abdf53
enock04 <e.ntwari@alustudent.com>
2025-03-17 20:05:00 +0000

    fixed

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -11,5 +11,6 @@ with urllib.request.urlopen(url) as response:
     content = response.read()
     print("Body response:")
     print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", content.decode("utf-8"))
+    print("\t- content:", expected_content)
+    print("\t- utf8 content:", expected_content.decode("utf-8"))
+
